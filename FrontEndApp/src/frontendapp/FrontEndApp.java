24540b6b2815eef0a879e060a3de8bef789f75ba
FernandoDelgado99 <fer.dres.del@gmail.com>
2019-02-05 18:45:22 -0600

    init files

---
+++ FrontEndApp/src/frontendapp/FrontEndApp.java
@@ -0,0 +1,18 @@
+/*
+ */
+package frontendapp;
+
+/**
+ *
+ * @author Clases
+ */
+public class FrontEndApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
